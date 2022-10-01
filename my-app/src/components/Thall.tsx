081d60c2d129f96661a249a39a12dbde37448954
evnwttn <evnwttn@gmail.com>
2022-10-01 13:23:42 -0230

    Update Thall.tsx

--- my-app/src/components/Thall.tsx
+++ my-app/src/components/Thall.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 import { Box, IconButton } from '@mui/material';
 
 export const Thall = () => {
+    const onClick = () => {
+        console.log('yo')
+    }
+
     return (
         <Box>
-            <IconButton>
+            <IconButton onClick={onClick}>
                 <AcUnitIcon />
             </IconButton>
         </Box>
