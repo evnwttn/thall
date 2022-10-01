175606a5194ce686119a409bc35d8ede60417026
evnwttn <evnwttn@gmail.com>
2022-10-01 16:29:58 -0230

    Update Thall.tsx

--- my-app/src/components/Thall.tsx
+++ my-app/src/components/Thall.tsx
@@ -2,6 +2,14 @@ import axios from 'axios';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 import { Box, IconButton } from '@mui/material';
 
+const buttonSx = {
+  position: "absolute",
+  color: "#ACD90B",
+  fontSize: "20",
+  top: "20vw",
+  left: "49vw",
+}
+
 export const Thall = () => {
     const onClick = () => {
         axios.post('http://localhost:4000/', 'yo')
@@ -15,7 +23,7 @@ export const Thall = () => {
 
     return (
         <Box>
-            <IconButton onClick={onClick}>
+            <IconButton sx={buttonSx} onClick={onClick}>
                 <AcUnitIcon />
             </IconButton>
         </Box>
